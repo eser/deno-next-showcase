45c6d3cb6f4d389b20d1e51c5a74b865e66deb4c
Eser Ozvataf <eser@acikyazilim.com>
2025-04-27 01:18:11 +0300

    ci: disabled edge runtime.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,22 @@
 import type { NextConfig } from "next";
 
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   // output: "standalone",
 
+  trailingSlash: false,
+  productionBrowserSourceMaps: true,
+  poweredByHeader: false,
+
   experimental: {
     reactCompiler: { compilationMode: "all" },
-    // reactCompiler: false,
-    // ppr: true, // 'incremental',
+    ppr: true, // 'incremental',
+    nodeMiddleware: true, // Enable Node.js middleware
   },
 };
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -24,9 +24,16 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
+  runtime: "nodejs",
   matcher: [
-    // Skip all internal paths (_next)
-    "/((?!_next).*)",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/|favicon.ico).*)",
     // Optional: only run on root (/) URL
     // '/'
   ],
